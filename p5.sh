bfba60362df213c6e37df7c4c656d9c8ffdbf718
Orangepi <777777@poczta.fm>
2023-11-13 17:40:41 +0100

    update3

--- p5.sh
+++ p5.sh
@@ -1,14 +1,14 @@
-sudo curl https://raw.githubusercontent.com/orangepi-xunlong/linux-orangepi/orange-pi-5.10-rk3588/arch/arm64/boot/dts/rockchip/overlay/rk3588-i2c5-m3.dts -o rk3588-i2c5-m3.dts
-sudo armbian-add-overlay rk3588-i2c5-m3.dts
-sudo curl https://raw.githubusercontent.com/orangepi-xunlong/linux-orangepi/orange-pi-5.10-rk3588/arch/arm64/boot/dts/rockchip/overlay/rk3588-uart0-m2.dts -o rk3588-uart0-m2.dts
-sudo armbian-add-overlay rk3588-uart0-m2.dts
+sudo curl https://raw.githubusercontent.com/orangepi-xunlong/linux-orangepi/orange-pi-5.10-rk3588/arch/arm64/boot/dts/rockchip/overlay/rk3588-i2c2-m0.dts -o rk3588-i2c2-m0.dts
+armbian-add-overlay rk3588-i2c2-m0.dts
+sudo curl https://raw.githubusercontent.com/orangepi-xunlong/linux-orangepi/orange-pi-5.10-rk3588/arch/arm64/boot/dts/rockchip/overlay/rk3588-uart6-m1.dts -o rk3588-uart6-m1.dts
+armbian-add-overlay rk3588-uart6-m1.dts
 sudo curl https://raw.githubusercontent.com/orangepi-xunlong/linux-orangepi/orange-pi-5.10-rk3588/arch/arm64/boot/dts/rockchip/overlay/rk3588-uart4-m0.dts -o rk3588-uart4-m0.dts
-sudo armbian-add-overlay rk3588-uart4-m0.dts
+armbian-add-overlay rk3588-uart4-m0.dts
 sudo apt-get install gpsd gpsd-clients minicom -y
 sudo adduser orangepi dialout
-echo "overlays=opi5-i2c5-m3 opi5-uart0-m2" | sudo tee -a /boot/armbianEnv.txt
-sudo systemctl stop serial-getty@ttyS0.service
-sudo systemctl disable serial-getty@ttyS0.service
+#echo "overlays=opi5-i2c5-m3 opi5-uart0-m2" | sudo tee -a /boot/armbianEnv.txt
+sudo systemctl stop serial-getty@ttyS6.service
+sudo systemctl disable serial-getty@ttyS6.service
 sudo cp 99-gps.rules /etc/udev/rules.d
 sudo cp -f gpsd /etc/default
 sudo reboot
