01a9b333af74e33f75c356c64779fc31ab138e9e
Orangepi <777777@poczta.fm>
2023-11-25 17:02:47 +0100

    update20

---
+++ block.sh
@@ -0,0 +1,6 @@
+sudo apt-mark hold openplotter-i2c
+sudo apt-mark hold openplotter-opencpn-installer
+sudo apt-mark hold openplotter-pypilot
+sudo apt-mark hold openplotter-sdr-vhf
+sudo apt-mark hold openplotter-signalk-installer
+sudo apt-mark hold openplotter-settings
