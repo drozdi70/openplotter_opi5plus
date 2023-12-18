0f254f08b7ecdfb4b0c20b89378e143eb014641b
Orangepi <777777@poczta.fm>
2023-12-18 10:04:40 +0100

    update89

--- p3A.sh
+++ p3A.sh
@@ -1,1 +1,2 @@
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 sudo curl https://raw.githubusercontent.com/TheRemote/PiBenchmarks/master/Storage.sh | sudo bash
